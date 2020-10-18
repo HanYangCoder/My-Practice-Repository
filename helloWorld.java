ec924fec96043bf2183c01f8f56f6ce396b74935
HansoYang647 <hanschristian.herrera@wvsu.edu.ph>
2020-10-18 16:07:28 +0800

    I added a simple score checker using an if-else statement

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class{
+    static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args){
+        int score;
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World! Please input your score: ");
+        score = scan.nextLine();
+
+        if(score == 100)
+        {
+            System.out.println("Congratulations! You've got a perfect score!");
+        }
+
+        else if((score <= 99) && (score >= 50))
+        {
+            System.out.println("Congratulations! You've passed!");
+        }
+
+        else{
+            System.out.println("Sorry you haven't passed, please try again next time!");
+        }
     }
 }
